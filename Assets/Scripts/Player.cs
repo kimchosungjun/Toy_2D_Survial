f0b3778d8105a060a81a90163da0ba754ad111cf
Sung Jun Cho <chosung0531@naver.com>
2023-01-25 16:09:02 +0900

    플레이어 이동
    플레이어 이동

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Vector2 inputVec;
+    public float speed;
+    Rigidbody2D rb;
+    SpriteRenderer spr;
+    Animator anim;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime; // fixedDeltaTime : 물리 프레임 하나가 소비한 시간, DeltaTime은 Update에서 사용
+        rb.MovePosition(rb.position + nextVec);
+    }
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed",inputVec.magnitude);
+        if (inputVec.x!=0)
+        {
+            spr.flipX = inputVec.x < 0 ? true : false;
+        }    
+    }
+}
